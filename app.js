41c4bf3940712ffcd7b06b44d3d14a67351e9680
Jan Gross <jan.gross@regiojet.cz>
2024-02-07 10:53:18 +0100

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const squares = document.querySelectorAll(".square")
+const time = document.getElementById("time")
 
-const showTheMole = () => {
+const runGame = () => {
   let randomNumber = Math.floor(Math.random() * 9)
   const randomSquare = squares[randomNumber].classList.add("mole")
+  setTimeout(() => {
+    squares[randomNumber].classList.remove("mole")
+  }, 800)
 }
-setInterval(showTheMole, 2000)
+setInterval(runGame, 2000)
+
+squares.forEach((square) => {
+  square.addEventListener("click", (e) => {
+    let clickOnMole = e.target.classList.contains("mole")
+    if (clickOnMole) {
+      let score = document.getElementById("score").innerHTML
+      score++
+      document.getElementById("score").innerHTML = score
+    }
+  })
+})
+
+const countDown = () => {
+  let currentTime = time.textContent
+  currentTime--
+  time.textContent = currentTime
+  if (currentTime === 0) {
+    clearInterval(timerId)
+    alert("Game over! Your final score is " + document.getElementById("score").innerHTML)
+  }
+}
+let timerId = setInterval(countDown, 1000)
